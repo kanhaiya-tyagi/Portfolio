461e5f2bba23e8a20d0b1742b831e5bfaf89f77b
Kanhaiya Tyagi <kanhaiya.tyagi.2006@gmail.com>
2025-06-19 09:19:38 +0530

    Fixed: expolre panel collapsing when something clicked

--- src/components/explore.jsx
+++ src/components/explore.jsx
@@ -20,12 +20,36 @@ const explore = () => {
 
 			<div className={`linkList ${showList? 'visible': ''}`}>
 				<ul>
-					<li><a href="#about" onClick={toggleList}><FaInfoCircle style={{marginRight: "8px"}} />about<span>&#x2197;</span></a></li>
-					<li><a href="#skills"><FaCode style={{marginRight: "8px"}} />skills<span>&#x2197;</span></a></li>
-					<li><a href="#projects"><FaProjectDiagram style={{marginRight: "8px"}} />projects<span>&#x2197;</span></a></li>
-					<li><a href="#certificates"><FaCertificate style={{marginRight: "8px"}} />certificates<span>&#x2197;</span></a></li>
-					<li><a href="#education"><FaGraduationCap style={{marginRight: "8px"}} />education<span>&#x2197;</span></a></li>
-					<li><a href="#footer"><FaComments style={{marginRight: "10px"}} />contacts<span>&#x2197;</span></a></li>
+					<li>
+						<a href="#about" onClick={toggleList}>
+							<FaInfoCircle style={{marginRight: "8px"}} />about<span>&#x2197;</span>
+						</a>
+					</li>
+					<li>
+						<a href="#skills" onClick={toggleList}>
+							<FaCode style={{marginRight: "8px"}} />skills<span>&#x2197;</span>
+						</a>
+					</li>
+					<li>
+						<a href="#projects" onClick={toggleList}>
+							<FaProjectDiagram style={{marginRight: "8px"}} />projects<span>&#x2197;</span>
+						</a>
+					</li>
+					<li>
+						<a href="#certificates" onClick={toggleList}>
+							<FaCertificate style={{marginRight: "8px"}} />certificates<span>&#x2197;</span>
+						</a>
+					</li>
+					<li>
+						<a href="#education" onClick={toggleList}>
+							<FaGraduationCap style={{marginRight: "8px"}} />education<span>&#x2197;</span>
+						</a>
+					</li>
+					<li>
+						<a href="#footer" onClick={toggleList}>
+							<FaComments style={{marginRight: "10px"}} />contacts<span>&#x2197;</span>
+						</a>
+					</li>
 				</ul>
 			</div>
 		</div>
